c1c14902f1a28bbbc807b556e6710ea661051b81
Mosheur <mosheur.rahman@yandex.com>
2022-09-25 09:17:30 +0600

    New File

---
+++ Addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Enter two number you want to add\n");
+    scanf("%d%d",&a, &b);
+
+    printf("Total is %d",c=a+b);
+    return 0;
+}
