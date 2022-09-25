57eb1fd147e1d482faba9156e727b9dfb2f5908d
Mosheur <mosheur.rahman@yandex.com>
2022-09-25 09:58:17 +0600

    New File

---
+++ Subtraction.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(void)
+{
+    float num1, num2, Subtraction;
+
+    printf("Enter Number 1:\t");
+    scanf("%f",&num1);
+    printf("\nEnter Number 2:\t");
+    scanf("%f",&num2);
+    printf("\nSubtraction is %f",Subtraction=num1/num2);
+    
+    return 0;
+
+}
